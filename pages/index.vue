5b378867b25f58ac396c07330ee9316283742d09
dev_lxa <766576780@qq.com>
2026-07-28 19:10:19 +0800

    feat(index, config): add collapse component and format config files
    1. add vant collapse panel component to index page with default active tab 1
    2. format all nuxt config code style with double quotes and consistent line breaks
    3. adjust vant importStyle configuration to use boolean true

--- pages/index.vue
+++ pages/index.vue
@@ -22,6 +22,21 @@
       </van-cell-group>
     </div>
 
+    <div class="p-4">
+      <div class="text-lg font-bold mb-3">折叠面板</div>
+      <van-collapse v-model="activeNames">
+        <van-collapse-item title="标题1" name="1">
+          代码是写出来给人看的，附带能在机器上运行。
+        </van-collapse-item>
+        <van-collapse-item title="标题2" name="2">
+          技术无非就是那些开发它的人的共同灵魂。
+        </van-collapse-item>
+        <van-collapse-item title="标题3" name="3">
+          在代码阅读过程中人们说脏话的频率是衡量代码质量的唯一标准。
+        </van-collapse-item>
+      </van-collapse>
+    </div>
+
     <div class="p-4 flex flex-col gap-4">
       <div class="text-lg font-bold mb-3">计数器 (Pinia Store)</div>
       <van-button plain type="primary" block @click="counter.decrement()">
@@ -44,7 +59,7 @@
 </template>
 
 <script setup lang="ts">
-import { useCounterStore } from "~/stores/counter";
-
 const counter = useCounterStore();
+
+const activeNames = ref(["1"]);
 </script>
